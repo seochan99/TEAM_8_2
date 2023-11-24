b03105bf26e55fba85cbf86c6f16ab61404a61a2
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-11-24 17:58:54 +0900

    :sparkles: Feat: query userName-> nameSetting
    feat: query userName-> nameSetting

---
+++ src/pages/PostBox/PostBox.jsx
@@ -0,0 +1,41 @@
+import * as S from "./style";
+import Logo from "../../assets/logo-shadow.png";
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+
+function PostBox() {
+  // location username=uuid 값을 가지고와서 변수에 저장
+  //   const location = useLocation();
+  //   const { username } = useParams();
+  const [userName, setUserName] = useState("chan");
+
+  // 나중에 query로 유저 정보 가져와서 저장
+  //   useEffect(() => {
+  //     first;
+
+  //     return () => {
+  //       second;
+  //     };
+  //   }, [third]);
+
+  const [post, setPost] = useState({
+    title: "",
+    content: "",
+    receiver: "",
+    sender: "",
+    letterImage: ""
+  });
+
+  return (
+    <S.Root>
+      <S.LogoImage src={Logo} alt="logo" />
+      <S.Title>
+        {userName}님의 우체통에 방문했어요.
+        <br />
+        편지 한 장 놓고 가실래요?
+      </S.Title>
+    </S.Root>
+  );
+}
+
+export default PostBox;
---
+++ src/pages/PostBox/style.jsx
@@ -0,0 +1,37 @@
+import { styled } from "styled-components";
+import Bg from "../../assets/img/background-1.png";
+import Lbox from "../../assets/img/letterbox-1.png";
+import btnYellow from "../../assets/img/btnYellow.png";
+import btnWhite from "../../assets/img/btnWhite.png";
+import btnIdpw from "../../assets/img/btnIDPW.png";
+
+export const Root = styled.div`
+  margin: 0 auto;
+  max-width: 430px;
+  width: 100%;
+  min-height: 100vh;
+  background: url(${Bg}), lightgray 50% / cover no-repeat;
+  background-size: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: black;
+`;
+
+export const LogoImage = styled.img`
+  width: 16%;
+  margin: 90px auto 48px auto;
+`;
+export const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Esamanru";
+  font-weight: 500; // semi-boldr가 있어야할거같음
+  font-size: 1.5rem;
+  color: white;
+  margin-bottom: 66px;
+  white-space: pre-wrap;
+  line-height: normal;
+  text-align: center;
+`;
